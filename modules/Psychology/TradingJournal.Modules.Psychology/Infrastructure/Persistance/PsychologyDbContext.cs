864ba72281b8e6667d6afbcd28f47194f20dee4c
LuongQuocKhang <luong.quockhang@amaris.com>
2026-06-05 07:32:57 +0700

    Update

--- modules/Psychology/TradingJournal.Modules.Psychology/Infrastructure/Persistance/PsychologyDbContext.cs
+++ modules/Psychology/TradingJournal.Modules.Psychology/Infrastructure/Persistance/PsychologyDbContext.cs
@@ -1,10 +1,9 @@
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using TradingJournal.Shared.Extensions;
+using TradingJournal.Shared.Infrastructure;
 
 namespace TradingJournal.Modules.Psychology.Infrastructure.Persistance;
 
 internal sealed class PsychologyDbContext(DbContextOptions<PsychologyDbContext> options, IHttpContextAccessor httpContextAccessor)
-    : DbContext(options), IPsychologyDbContext
+    : AuditableDbContext(options, httpContextAccessor), IPsychologyDbContext
 {
     public DbSet<EmotionTag> EmotionTags { get; set; } = null!;
 
@@ -21,26 +20,4 @@ internal sealed class PsychologyDbContext(DbContextOptions<PsychologyDbContext> 
     public DbSet<Achievement> Achievements { get; set; } = null!;
 
     public DbSet<DailyNote> DailyNotes { get; set; } = null!;
-
-    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-    {
-        int userId = httpContextAccessor.HttpContext?.User.GetCurrentUserId() ?? 0;
-
-        foreach (EntityEntry<EntityBase<int>> entry in ChangeTracker.Entries<EntityBase<int>>())
-        {
-            switch (entry.State)
-            {
-                case EntityState.Added:
-                    entry.Entity.CreatedDate = DateTime.UtcNow;
-                    entry.Entity.CreatedBy = userId;
-                    break;
-                case EntityState.Modified:
-                    entry.Entity.UpdatedDate = DateTime.UtcNow;
-                    entry.Entity.UpdatedBy = userId;
-                    break;
-            }
-        }
-
-        return base.SaveChangesAsync(cancellationToken);
-    }
 }
